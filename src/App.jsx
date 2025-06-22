853533a6c87e3b5f60487e58e046bf88bf7bb315
anaclaraleitexavier <ana.xavier67@aluno.ce.gov.br>
2025-06-22 17:55:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Cadastro from './componentes/cadastro'
 import Login from './componentes/login'
 import Home from './componentes/home'
@@ -7,9 +7,10 @@ import Cadnotas from './componentes/cadnotas'
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/cadnotas" element={<Cadnotas />} />
     </Routes>
   )
