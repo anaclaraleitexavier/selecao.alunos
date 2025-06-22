b88df7444a7eb520be0d95ddbdea1885d66865b4
anaclaraleitexavier <ana.xavier67@aluno.ce.gov.br>
2025-06-22 17:45:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Cadastro from './componentes/cadastro'
 import Login from './componentes/login'
 import Home from './componentes/home'
 import Cadnotas from './componentes/cadnotas'
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <Cadastro/>
-    <Login/>
-    <Home/>
-    <Cadnotas/>
-
-    </>
- 
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadnotas" element={<Cadnotas />} />
+    </Routes>
   )
 }
 
 export default App
- 
